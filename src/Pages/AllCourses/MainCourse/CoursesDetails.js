8440c0d003ab9ed15efb81114d1c55a57069bfe6
Subirdas29 <dasjoy104@gmail.com>
2022-10-27 12:51:10 +0600

    react to pdf added

--- src/Pages/AllCourses/MainCourse/CoursesDetails.js
+++ src/Pages/AllCourses/MainCourse/CoursesDetails.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaDownload } from "react-icons/fa";
+import ReactToPdf from 'react-to-pdf'
 
 const CoursesDetails = () => {
     const coursesDetails = useLoaderData()
-    const {title,image_url,details} =coursesDetails;
+    const {title,image_url,details,name} =coursesDetails;
+
+    const ref = React.createRef();
     return (
         
-          <div>
+      // <FaDownload className='text-3xl text-center'></FaDownload>
             
+            <div className='flex flex-col'>
+    <ReactToPdf targetRef={ref} filename={`${name}.pdf`}>
+        {({toPdf}) => (
+            <button className='className="btn btn-outline btn-accent mx-96' onClick={toPdf}>pdf download</button>
+        )}
+    </ReactToPdf>
+    
 
-            <div className='flex justify-center items-center '>
+            <div ref={ref}>
+            <div className='flex justify-center items-center'>
             <h1 className='text-4xl font-bold text-center my-10 underline mx-4'>{title}</h1>
-            <FaDownload className='text-3xl'></FaDownload>
+
             </div>
               <div className="card w-96 bg-base-100 shadow-xl mx-auto">
   <figure><img src={image_url}/></figure>
@@ -26,7 +37,7 @@ const CoursesDetails = () => {
   </div>
 </div>
           </div>
-       
+         </div>
     );
 };
 
