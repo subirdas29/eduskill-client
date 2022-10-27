9f9230b9d655f1384a414cbfab6b23e8e2a42707
Subirdas29 <dasjoy104@gmail.com>
2022-10-27 18:59:46 +0600

    banner responsive

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -7,6 +7,7 @@ import DarKAndLightMode from '../DarkAndLightMode/DarKAndLightMode';
 
 
 
+
 const Header = () => {
   const { user, logOut } = useContext(AuthContext)
 
@@ -19,27 +20,28 @@ const Header = () => {
       });
   }
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
+    <div className="navbar bg-[#140342] border-b-2 p-4 border-[#007C86]">
+      <div className="navbar-start ">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/courses'>Courses</Link></li>
-            <li><Link to='/blog'>Blogs</Link></li>
-            <li><Link to='/faq'>FAQ</Link></li>
+            <li className='text-white'><Link to='/'>Home</Link></li>
+            <li className='text-white'><Link to='/courses'>Courses</Link></li>
+            <li className='text-white'><Link to='/blog'>Blogs</Link></li>
+            <li className='text-white'><Link to='/faq'>FAQ</Link></li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">EduSkill</a>
+       
+        <Link to='/' className=" text-3xl text-white ml-6">EduSkill</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/courses'>Courses</Link></li>
-          <li><Link to='/blog'>Blogs</Link></li>
-          <li><Link to='/faq'>FAQ</Link></li>
+          <li className='text-white'><Link to='/'>Home</Link></li>
+          <li className='text-white'><Link to='/courses'>Courses</Link></li>
+          <li className='text-white'><Link to='/blog'>Blogs</Link></li>
+          <li className='text-white'><Link to='/faq'>FAQ</Link></li>
 
         </ul>
       </div>
@@ -51,12 +53,12 @@ const Header = () => {
               user?.uid ?
                 <>
                   <button onClick={logOutBtn} className="btn mx-3">Logout</button>
-                  <span>{user?.displayName}</span>
+                  <span className='text-white'>{user?.displayName}</span>
                 </>
                 :
                 <>
-                  <Link to="/login" className="btn">Login</Link>
-                  <Link to="/register" className="btn mx-3">Register</Link>
+                  <Link to="/login" className="btn btn-outline btn-success">Login</Link>
+                  <Link to="/register" className="btn btn-outline btn-success mx-3">Register</Link>
                 </>
             }
           
